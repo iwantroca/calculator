3d0c2b7f68cf797760500cf918d5c4316a22f33b
Iwantroca <mohitnotiyal@yahoo.com>
2021-08-09 21:07:44 +0530

    Fix the function to handle second input

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll("button");
 const input = document.getElementById("input");
 const operatorsNode = document.querySelectorAll(".operator");
 const equalButton = document.querySelector("#equal-btn");
+const resultDisplay = document.querySelector("#result");
 let allNumbers = [];
 let operatorsValue = [...operatorsNode].map((x) => x.textContent);
 let operatorPosition;
@@ -9,6 +10,7 @@ let operatorPosition;
 let num1;
 let num2;
 let currentOperator;
+let result = 0;
 
 function getInput(e) {
   allNumbers.push(e.target.textContent);
@@ -17,10 +19,10 @@ function getInput(e) {
 function findOperatorPosition(arr) {
   operatorPosition = arr.findIndex((x) => operatorsValue.includes(x));
   if (operatorPosition >= 0 && operatorPosition != arr.length - 1) {
-    num2 = allNumbers.slice(operatorPosition + 1);
-    currentOperator = allNumbers.slice(operatorPosition);
+    num2 = parseInt(allNumbers.slice(operatorPosition + 1).join(""));
+    currentOperator = allNumbers[operatorPosition];
   }
-  num1 = parseInt(allNumbers.slice(0, operatorPosition).join(""));
+  num1 = result || parseInt(allNumbers.slice(0, operatorPosition).join(""));
 }
 
 buttons.forEach((button) => button.addEventListener("click", getInput));
