0df28f37d17fefa1f870e01e4775c8e92cc363dc
Iwantroca <mohitnotiyal@yahoo.com>
2021-08-12 21:36:53 +0530

    Update the logic and refactor the code

--- script.js
+++ script.js
@@ -1,88 +1,66 @@
-const buttons = document.querySelectorAll("button");
-const input = document.getElementById("input");
-const operatorsNode = document.querySelectorAll(".operator");
-const equalButton = document.querySelector("#equal-btn");
-const resultDisplay = document.querySelector("#result");
-const decimalButton = document.querySelector("#decimal-btn");
-let allNumbers = [];
-let operatorsValue = [...operatorsNode].map((x) => x.value);
-let operatorPosition;
-let num1;
-let num2;
-let currentOperator;
+let firstOperand = "";
+let secondOperand = "";
+let currentOperator = "";
 let result = 0;
+let lastOperator = "";
 
-function getInput(e) {
-  allNumbers.push(e.target.value);
-  findOperatorPosition(allNumbers);
-  splitOperators(allNumbers);
-}
-function addDecimal(e) {
-  allNumbers.push(".");
-}
-function findOperatorPosition(arr) {
-  operatorPosition = arr.findIndex((x) => operatorsValue.includes(x));
-  if (operatorPosition >= 0) {
-    for (let i = arr.length - 1; i >= 0; --i) {
-      if (operatorsValue.includes(arr[i])) {
-        operatorPosition = i;
-        break;
-      }
-    }
-  }
-  return operatorPosition;
-}
-function clearAllInput() {
-  allNumbers = [];
-  currentOperator = "";
-  num2 = "";
-}
-function splitOperators(arr) {
-  // if (isNaN(num2)) num2 = "";
-  if (operatorPosition >= 0) {
-    // if (operatorPosition >= 0 && operatorPosition != arr.length - 1) {
-    num2 = parseFloat(allNumbers.slice(operatorPosition + 1).join(""));
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const operators = [...operatorButtons].map((x) => x.textContent);
+const lastDisplay = document.querySelector("#display-last");
+const currentDisplay = document.querySelector("#display-current");
+const equalButton = document.querySelector("#equal-btn");
 
-    currentOperator = allNumbers[operatorPosition];
-  }
-  //parseInt(allNumbers.slice(0, operatorPosition).join(""))
-  num1 = result || parseFloat(allNumbers.slice(0).join(""));
-  if (isNaN(num2)) {
-    console.log(`${num1} ${currentOperator ?? ""}`);
-  } else {
-    console.log(`${num1} ${currentOperator ?? ""} ${num2 ?? ""}`);
-  }
+// add eventlisterner to operators buttons so that if currentDisplay is true and
+// firstOperand is true
+// then take the new Operator and textcontent as second operand
+
+function getNumbers(num) {
+  currentDisplay.textContent += num;
+}
+function getOperator(operator) {
+  // firstOperand = currentDisplay.textContent || result;
+  firstOperand = result || currentDisplay.textContent;
+
+  currentOperator = operator;
+  updateLastDisplay();
+  currentDisplay.textContent = "";
 }
 function doCalculation(operator, input1, input2) {
   switch (operator) {
     case "+":
-      result = input1 + input2;
+      result = Number(input1) + Number(input2);
       break;
     case "-":
-      result = input1 - input2;
+      result = Number(input1) - Number(input2);
       break;
-    case "*":
-      result = input1 * input2;
+    case "×":
+      result = Number(input1) * Number(input2);
       break;
-    case "/":
-      result = input1 / input2;
-      break;
-    case "%":
-      result = input1 / 100;
-      break;
-    default:
-      // console.log("error");
-      result = num1;
   }
 }
+function updateLastDisplay() {
+  lastDisplay.textContent = `${firstOperand} ${currentOperator} ${secondOperand}`;
+}
 
-decimalButton.addEventListener("click", addDecimal);
-buttons.forEach((button) => button.addEventListener("click", getInput));
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    getNumbers(button.textContent);
+  })
+);
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    getOperator(button.textContent);
+  })
+);
 equalButton.addEventListener("click", () => {
-  if (!isNaN(num2)) {
-    doCalculation(currentOperator, num1, num2);
-  } else {
-    result = num1;
+  if (currentOperator) {
+    secondOperand = currentDisplay.textContent;
+    doCalculation(currentOperator, firstOperand, secondOperand);
+    currentDisplay.textContent = result;
+    updateLastDisplay();
+    firstOperand = result;
+    secondOperand = "";
+    currentOperator = "";
   }
-  clearAllInput();
 });
